271b4679a5f853e8c939b72d8868d71ad0b53803
Camille <92870357+cmllnnmrc@users.noreply.github.com>
2024-08-26 14:25:11 +0800

    Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -32,7 +32,7 @@ const Home = () => {
             child: (
                 <HiOutlineMail size={30} />
             ),
-            href: 'mailto:cmarca01@gmail.com'
+            href: 'https://mail.google.com/mail/?view=cm&fs=1&to=cmarca01@gmail.com'
         },
     ]
 
