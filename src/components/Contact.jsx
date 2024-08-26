f6936752333ca5c6466fc956dd08e83137ee58db
Camille <92870357+cmllnnmrc@users.noreply.github.com>
2024-08-26 14:27:46 +0800

    Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,7 +7,7 @@ const Contact = () => {
             <div className="contact__wrapper">
                 <h2>I'm enthusiastic about partnering</h2>
                 <p>Feel free to share your ideas without hesitation.</p>
-                <a href='mailto:cmarca01@gmail.com' className='btn white'>Contact Me</a>
+                <a href='https://mail.google.com/mail/?view=cm&fs=1&to=cmarca01@gmail.com' className='btn white'>Contact Me</a>
             </div>
         </div>
     </section>
